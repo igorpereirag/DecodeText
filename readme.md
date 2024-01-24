6c11d22b663479812374206aafce414ca4cd2548
IGOR <igorpereira1501@gmail.com>
2024-01-24 02:00:25 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Decodificador de Texto
+
+Este projeto consiste em uma aplicação simples para codificar e decodificar textos usando JavaScript. Ele oferece uma interface amigável para inserir texto, codificá-lo ou decodificá-lo e copiar o resultado para a área de transferência.
+
+## Funcionalidades
+
+- **Codificação:** Transforma o texto inserido em sua forma codificada usando base64.
+- **Decodificação:** Converte o texto codificado de volta à sua forma original.
+- **Cópia para Área de Transferência:** Permite copiar facilmente o resultado para a área de transferência.
+
+## Como Usar
+
+1. Insira o texto na caixa de texto.
+2. Escolha entre codificar ou decodificar usando os botões correspondentes.
+3. Copie o resultado clicando no botão "Copiar para a Área de Transferência".
+
+## Pré-requisitos
+
+Certifique-se de ter um navegador da web atualizado para a melhor experiência.
+
+## Contribuição
+
+Contribuições são bem-vindas! Sinta-se à vontade para abrir problemas ou enviar solicitações de pull.
+
+## Autor
+
+Desenvolvido por: [Igor pereira](https://github.com/Igorpereirag)
+
+
+
---
+++ script.js
@@ -0,0 +1,47 @@
+function  codificarTexto() {
+    try {
+      var texto = document.getElementById('inputText').value;
+      var textoCodificado = btoa(texto);
+      document.getElementById('resultMessage').innerText = textoCodificado;
+      document.getElementById('textoStatus').innerText = " Texto Codificado:";
+      document.getElementById('inputText').value = "";
+    } catch (error) {
+      document.getElementById('resultMessage').innerText = "Erro ao codificar o texto.";
+      console.error("Erro ao codificar o texto:", error);
+    }
+  }
+  
+  
+  function descodificarTexto() {
+    try {
+      var texto = document.getElementById('inputText').value;
+      var textoDecodificado = atob(texto);
+      document.getElementById('resultMessage').innerText = textoDecodificado;
+      document.getElementById('textoStatus').innerText = " Texto Descodificado:";
+      document.getElementById('inputText').value = "";
+      
+    } catch (error) {
+      document.getElementById('resultMessage').innerText = "Erro ao decodificar o texto.";
+      console.error("Erro ao decodificar o texto:", error);
+    }
+  }
+  
+  function copy() {
+    try {
+      var resultTextElement = document.getElementById('resultMessage');
+      var textArea = document.createElement('textarea');
+    
+      textArea.value = resultTextElement.innerText;
+      document.body.appendChild(textArea);
+    
+      textArea.select();
+      document.execCommand('copy');
+    
+      document.body.removeChild(textArea);
+     console.log("Texto copiado para a Área de Transferência.");
+     
+    } catch (error) {
+      console.error("Erro ao copiar para a Área de Transferência:", error);
+    }
+  }
+  
